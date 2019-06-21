d3af215f42fda83be05b48a696ca226d379778f1
Alex Lang <me@alang.ca>
2019-06-21 16:56:34 -0400

    Fix bounded DFS to actually work

--- lib/ICFP2019/DFS.hs
+++ lib/ICFP2019/DFS.hs
@@ -1,21 +1,54 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
 module ICFP2019.DFS where
 
-import Control.Arrow
+import Control.Lens
+import qualified Data.HashSet as HS
 import Data.List
+import Data.Maybe
 import Data.Ord
 import Data.Sequence (Seq)
 import qualified Data.Sequence as Seq
+import Linear
 
 import ICFP2019.Action
 import ICFP2019.State
 
 dfsOneStep :: MineState -> [(Action, MineState)]
-dfsOneStep state0 = map (\act -> (act, either (\ex -> error $ "dfsOneStep " ++ show ex) id $ step state0 act )) $ interestingActions state0
+dfsOneStep state0 = mapMaybe go $ interestingActions state0
+  where
+    go act = case step state0 act of
+      Left ex -> error $ "dfsOneStep: " ++ show ex
+      Right state1 -> Just (act, state1)
 
-boundedDfs :: (MineState -> Double) -> Int -> MineState -> (Seq Action, MineState)
-boundedDfs fitness maxDepth initial
-  | allWrapped initial = (mempty, initial)
-  | maxDepth <= 0 = (mempty, initial)
-  | otherwise = case dfsOneStep initial of
-      [] -> (mempty, initial)
-      xs@(_:_) -> minimumBy (comparing $ \(acts, st) -> (fitness st, length acts) )$ map (\(act, st) -> first (act Seq.:<|) $ boundedDfs fitness (maxDepth - 1) st) xs
+boundedDfs :: forall m a. (Monad m, Ord a) => (MineState -> m a) -> Int -> MineState -> m (Seq Action, MineState)
+boundedDfs fitness maxDepth initial = do
+  (acts, st, _) <- go maxDepth initial
+  return (acts, st)
+  where
+  go :: Int -> MineState -> m (Seq Action, MineState, a)
+  go !remDepth !st
+    | allWrapped st = do
+        !fit <- fitness st
+        return (mempty, st, fit)
+    | remDepth <= 0 = do
+        !fit <- fitness st
+        return (mempty, st, fit)
+    | otherwise = case dfsOneStep st of
+        [] -> do
+          !fit <- fitness st
+          return (mempty, st, fit)
+        xs@(_:_) -> do
+          xs' <- mapM (\(act, st') -> (_1 %~ (act Seq.:<|)) <$> go (remDepth - 1) st') xs
+          return $ minimumBy (comparing $ \(_, _, score) -> score) xs'
+  -- fitness' a = (\b -> (b, costHeuristic a)) <$> fitness a
+
+costHeuristic :: MineState -> Int
+costHeuristic state0 = case map go $ HS.toList $ missingTiles state0 of
+    [] -> 0
+    xs@(_:_) -> minimum xs
+  where
+    V2 x y = state0 ^. wwPosition
+    go (V2 x' y') = abs (x - x') + abs (y - y')
--- lib/ICFP2019/Main/Dfs.hs
+++ lib/ICFP2019/Main/Dfs.hs
@@ -4,6 +4,7 @@ import qualified Data.Attoparsec.ByteString as AP
 import qualified Data.ByteString.Char8 as C8
 import qualified Data.Sequence as Seq
 import System.Environment
+import System.IO
 
 import ICFP2019.State
 import ICFP2019.Action
@@ -15,13 +16,16 @@ main = do
   descBs <- C8.readFile desc
   let Right state0 = AP.parseOnly initialParser descBs
   go state0
+  putStrLn ""
   where
     go st
-      | allWrapped st = return ()
-      | otherwise =
-          case boundedDfs (fromIntegral . remainingTiles) 5 st of
+      | allWrapped st = putStrLn ""
+      | otherwise = do
+          foo <- boundedDfs (\st -> pure (remainingTiles st, costHeuristic st)) 5 st
+          case foo of
             (x Seq.:<| _, _) -> do
-              putStrLn $ serialize x
+              putStr $ serialize x
+              hFlush stdout
               go (either (error "impossible") id $ step st x)
 --   go state0 undefin
 
