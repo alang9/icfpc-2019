e9ce0a7aa45d4d29c452276a30acf3978ec8e7bf
Jasper Van der Jeugt <m@jaspervdj.be>
2019-06-23 14:38:29 +0200

    Tweak stdin/stdout usage in Sim

--- lib/ICFP2019/Main/Sim.hs
+++ lib/ICFP2019/Main/Sim.hs
@@ -7,16 +7,17 @@ import qualified Data.Attoparsec.ByteString as AP
 import qualified Data.ByteString.Char8 as C8
 import System.Environment
 import Data.HashMap.Lazy as HM
+import qualified System.IO as IO
 
 import ICFP2019.State
 import ICFP2019.Action
 
 import Debug.Trace
 
-printState :: FullState -> HM.HashMap Int [Action] -> IO ()
-printState state actions = do
-  print nextActions
-  print workers
+printState :: IO.Handle -> FullState -> HM.HashMap Int [Action] -> IO ()
+printState h state actions = do
+  IO.hPrint h nextActions
+  IO.hPrint h workers
   where
     nextActions = HM.map (\l -> 
       case l of 
@@ -34,18 +35,19 @@ main = do
         AP.Fail _ _ _ -> error "parsing failed" 
         AP.Partial _ -> error "unexpected partial"
         AP.Done remain actions -> actions 
-  print $ state0 ^. fBoosters
+  IO.hPutStrLn IO.stderr $ show $ state0 ^. fBoosters
   go prob 0 state0 actions
   where
     go :: MineProblem -> Int -> FullState -> HashMap Int [Action] -> IO ()
     go prob n state0 actions = do
       if allWrapped state0
         then do
-          putStrLn $ "Completed after " ++ show n ++ " steps"
-          putStrLn $ "Remaining actions: " ++ show actions
-          putStrLn $ "Misssing tiles: " ++ show (missingTiles state0)
+          IO.hPutStrLn IO.stderr $ "Completed after " ++ show n ++ " steps"
+          IO.hPutStrLn IO.stderr $ "Remaining actions: " ++ show actions
+          IO.hPutStrLn IO.stderr $ "Misssing tiles: " ++ show (missingTiles state0)
+          print n
         else do
-          printState state0 actions 
+          printState IO.stderr state0 actions
           case stepAllWorkers prob state0 actions of
               Left exc -> error $ "sim exception: " ++ show exc
               Right (state1, act1) -> do
